2c4f1a97b878399cee8c26e0e87115f3f9f9d78f
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2023-06-18 01:26:43 +0300

    some includes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from "./lib/translation-repository";
 export * from "./lib/model";
+export * from "./lib/translation-repository";
+export * from "./lib/environment-controller";
 export * from "./lib/translation-lazyload-helper";
